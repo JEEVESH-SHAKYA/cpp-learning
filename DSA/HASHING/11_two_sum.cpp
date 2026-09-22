0a14d9045bfd8e0e86fc4be8351a202925a979d1
Jivesh Shakya <YOUR_GITHUB_EMAIL>
2026-09-22 20:09:14 +0530

    Add two sum hashing problem

--- DSA/HASHING/11_two_sum.cpp
+++ DSA/HASHING/11_two_sum.cpp
@@ -1,12 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int a;
+    int a,k;
     cin >>a;
     int x[a];
     for(int i=0;i<a;i++){
-    cin >>x[i];
+        cin >>x[i];
     }
+    cin >>k;
     int mx=x[0];
     for(int i=0;i<a;i++){
         if(x[i]>mx)mx=x[i];
@@ -16,11 +17,11 @@ int main(){
         hash[x[i]]+=1;
     }
     for(int i=0;i<a;i++){
-        if(hash[x[i]]>(a/2)){
-            cout << x[i];
+        if(hash[k-x[i]]>0 && (k-x[i])!=x[i]){
+            cout <<"YES";
             return 0;
         }
     }
-
+    cout <<"NO";
     return 0;
 }
